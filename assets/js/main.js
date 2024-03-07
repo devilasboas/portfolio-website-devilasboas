2f62af90a4e323128f09341ecce116cd963f5475
camila de vilas boas <camiladevilasboas@gmail.com>
2024-03-07 12:13:50 -0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,32 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    //serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_fd914um','template_zrmop6m','#contact-form','QzUnJo8nL4jWT-W5f')
+        .then(() => {
+            //Show sent message
+            contactMessage.textContent = 'Mensagem enviada com sucesso ✅'
+
+            //Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            //Clear input fields
+            contactForm.reset()
+        },() => {
+            //Show error message
+            contactMessage.textContent = 'Mensagem não enviada (service error) ❌'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
